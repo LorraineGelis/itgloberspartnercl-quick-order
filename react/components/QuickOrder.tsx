cddc2ea158a23d02e92ac44d22dfc2ee47ef727d
Lorraine <lgelisdiaz@outlook.com>
2023-02-09 20:19:47 -0500

    feat: the css styles for the quick order component are done

--- react/components/QuickOrder.tsx
+++ react/components/QuickOrder.tsx
@@ -2,8 +2,27 @@ import React, { useState, useEffect } from 'react'
 import { useMutation, useLazyQuery } from 'react-apollo'
 import UPDATE_CART from '../graphql/updateCart.graphql'
 import GET_PRODUCT from '../graphql/getProductBySku.graphql'
+import { useCssHandles } from 'vtex.css-handles'
+
+import './styles.css'
 
 const QuickOrder = () => {
+
+    //Handles para css
+
+    const CSS_HANDLES = [
+        "quickOrder__container",
+        "quickOrder__main--title",
+        /* "quickOrder__formulario", */
+        "quickOrder__form--main-container",
+        "quickOrder__container--label",
+        /*   "quickOrder__label", */
+        "quickOrder__input",
+        /* "quickOrder__container--inputAndBtn", */
+        "quickOrder__btn-submit",
+    ]
+
+    const handles = useCssHandles(CSS_HANDLES)
 
     const [inputText, setInputText] = useState("");
     const [search, setSearch] = useState("")
@@ -59,14 +78,14 @@ const QuickOrder = () => {
         }
     }
 
-    return <div>
-        <h2>Compra rápida en COMPRAGAMER</h2>
+    return <div className={handles["quickOrder__container"]}>
+        <h2 className={handles["quickOrder__main--title"]}>Compra rápida en COMPRAGAMER</h2>
         <form onSubmit={searchProduct}>
-            <div>
-                <label htmlFor='sku'>Ingresa el número de SKU</label>
+            <div className={handles["quickOrder__form--main-container"]}>
+                <label className={handles["quickOrder__container--label"]} htmlFor='sku'>Ingresa el número de SKU</label>
                 <input id='sku' type='text' onChange={handleChange}></input>
             </div>
-            <input type="submit" value="Sumar al carrito" />
+            <input className={handles["quickOrder__btn-submit"]} type="submit" value="SUMAR AL CARRITO" />
         </form>
     </div>
 }
